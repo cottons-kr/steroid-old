eedc761791f091c1f415d87cf6b4169bcc912927
cottons-kr <kimtaeyoung0205@naver.com>
2022-03-23 18:59:28 +0900

    commit

--- src/script/loadYoutubeList.ts
+++ src/script/loadYoutubeList.ts
@@ -1,17 +1,18 @@
 import * as puppeteer from "puppeteer"
 
+type jsObject = {
+    [index: string]: any
+}
+
 const youtubeList = document.querySelector<HTMLElement>("#youtubeList")
 const youtubeListInputPopup = document.querySelector<HTMLElement>("#youtubeListInputPopup")
+const youtubeLinkInput = youtubeListInputPopup.querySelector<HTMLInputElement>("input")
 
 let num = 0
 youtubeList.addEventListener("click", () => {
     num += 1
-    if (num % 2 == 0) {
-        youtubeListInputPopup.style.display = "none"
-    }
-    else {
-        youtubeListInputPopup.style.display = "block"
-    }
+    if (num % 2 == 0) { youtubeListInputPopup.style.display = "none" }
+    else { youtubeListInputPopup.style.display = "block" }
 })
 
 async function scrollToBtm(page: puppeteer.Page) {
@@ -34,9 +35,31 @@ async function scrollToBtm(page: puppeteer.Page) {
 
 async function getYoutubeList(url: string="https://www.youtube.com/playlist?list=PLC7IbGRZ5AEjMZhKR1p6b_CpOqHWYI2Sj") {
     const mobile = puppeteer.devices["iPhone 6"]
-    const browser = await puppeteer.launch({headless: false})
+    const browser = await puppeteer.launch({headless: true})
     const page = await browser.newPage()
     await page.emulate(mobile)
     await page.goto(url)
     await scrollToBtm(page)
+
+    const videos = await page.$eval("#app > div.page-container > ytm-browse > ytm-single-column-browse-results-renderer > div > div > ytm-section-list-renderer > lazy-list > ytm-item-section-renderer > lazy-list > ytm-playlist-video-list-renderer",
+    element => {
+        let list: jsObject = {}
+        for (let video of element.childNodes) {
+            // @ts-ignore
+            const name: string = video.querySelector("h4").innerText; const link = video.querySelector("a").href
+            list[name] = link
+        }
+        return list
+    })
+    return videos
 }
+
+youtubeLinkInput.addEventListener("change", async () => {
+    const link = youtubeLinkInput.value
+    youtubeLinkInput.value = ""
+    let videos: jsObject = {}
+    await getYoutubeList(link)
+    .then(res => {videos = JSON.parse(JSON.stringify(res))})
+    .catch(err => {alert(err)})
+    console.log(videos)
+})
